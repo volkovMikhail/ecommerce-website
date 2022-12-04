3b2480c505b64aaffbcf3a812b109cd5faff0eec
Mikhail-Volkau <mv@bam-boo.eu>
2022-12-04 06:25:12 +0100

    first commit

---
+++ models/category.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const slug = require("mongoose-slug-updater");
+
+mongoose.plugin(slug);
+
+const categorySchema = Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    unique: true,
+    slug: "title",
+  },
+  imageUrl: {
+    type: String,
+    unique: false,
+  },
+});
+
+module.exports = mongoose.model("Category", categorySchema);
