2468ef5a6badef5b5c12ff3358b34171efd06725
Mikhail-Volkau <mv@bam-boo.eu>
2023-03-12 21:08:32 +0300

    add sorting products

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,12 +1,21 @@
 // fade out for flash messages
 setTimeout(function () {
-  $("#flash-msg").fadeOut("slow");
+  $('#flash-msg').fadeOut('slow');
 }, 3000);
 
 setTimeout(function () {
-  $("#success").fadeOut("slow");
+  $('#success').fadeOut('slow');
 }, 3000);
 
 setTimeout(function () {
-  $("#error").fadeOut("slow");
+  $('#error').fadeOut('slow');
 }, 3000);
+
+const sortSelect = document.querySelector('#sortSelect');
+
+sortSelect.addEventListener('change', (e) => {
+  const sortKey = e.target.value;
+  const url = new URL(window.location.href);
+  url.searchParams.set('sort', sortKey);
+  window.location.href = url;
+});
--- routes/products.js
+++ routes/products.js
@@ -1,115 +1,146 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Product = require("../models/product");
-const Category = require("../models/category");
-var moment = require("moment");
+const Product = require('../models/product');
+const Category = require('../models/category');
+var moment = require('moment');
 
 // GET: display all products
-router.get("/", async (req, res) => {
-  const successMsg = req.flash("success")[0];
-  const errorMsg = req.flash("error")[0];
+router.get('/', async (req, res) => {
+  const successMsg = req.flash('success')[0];
+  const errorMsg = req.flash('error')[0];
   const perPage = 8;
   let page = parseInt(req.query.page) || 1;
+  const sort = req?.query?.sort || '-createdAt';
   try {
     const products = await Product.find({})
-      .sort("-createdAt")
+      .sort(sort)
       .skip(perPage * page - perPage)
       .limit(perPage)
-      .populate("category");
+      .populate('category');
 
     const count = await Product.count();
 
-    res.render("shop/index", {
-      pageName: "All Products",
+    let queryParams = Object.entries(req.query)
+      .filter((entry) => entry[0] !== 'page')
+      .map((entry) => entry.join('='))
+      .join('&');
+
+    if (queryParams) queryParams += '&';
+
+    res.render('shop/index', {
+      sort,
+      pageName: 'All Products',
       products,
       successMsg,
       errorMsg,
       current: page,
       breadcrumbs: null,
-      home: "/products/?",
+      home: '/products/?' + queryParams,
       pages: Math.ceil(count / perPage),
     });
   } catch (error) {
     console.log(error);
-    res.redirect("/");
+    res.redirect('/');
   }
 });
 
 // GET: search box
-router.get("/search", async (req, res) => {
+router.get('/search', async (req, res) => {
   const perPage = 8;
   let page = parseInt(req.query.page) || 1;
-  const successMsg = req.flash("success")[0];
-  const errorMsg = req.flash("error")[0];
-
+  const successMsg = req.flash('success')[0];
+  const errorMsg = req.flash('error')[0];
+  const sort = req?.query?.sort || '-createdAt';
   try {
     const products = await Product.find({
-      title: { $regex: req.query.search, $options: "i" },
+      title: { $regex: req.query.search, $options: 'i' },
     })
-      .sort("-createdAt")
+      .sort(sort)
       .skip(perPage * page - perPage)
       .limit(perPage)
-      .populate("category")
+      .populate('category')
       .exec();
     const count = await Product.count({
-      title: { $regex: req.query.search, $options: "i" },
+      title: { $regex: req.query.search, $options: 'i' },
     });
-    res.render("shop/index", {
-      pageName: "Search Results",
+
+    let queryParams = Object.entries(req.query)
+      .filter((entry) => entry[0] !== 'page')
+      .map((entry) => entry.join('='))
+      .join('&');
+
+    if (queryParams) queryParams += '&';
+
+    res.render('shop/index', {
+      sort,
+      pageName: 'Search Results',
       products,
       successMsg,
       errorMsg,
       current: page,
       breadcrumbs: null,
-      home: "/products/search?search=" + req.query.search + "&",
+      home: '/products/search?' + queryParams,
       pages: Math.ceil(count / perPage),
     });
   } catch (error) {
     console.log(error);
-    res.redirect("/");
+    res.redirect('/');
   }
 });
 
 //GET: get a certain category by its slug (this is used for the categories navbar)
-router.get("/:slug", async (req, res) => {
-  const successMsg = req.flash("success")[0];
-  const errorMsg = req.flash("error")[0];
+router.get('/:slug', async (req, res) => {
+  const successMsg = req.flash('success')[0];
+  const errorMsg = req.flash('error')[0];
   const perPage = 8;
   let page = parseInt(req.query.page) || 1;
+  const sort = req?.query?.sort || '-createdAt';
   try {
     const foundCategory = await Category.findOne({ slug: req.params.slug });
     const allProducts = await Product.find({ category: foundCategory.id })
-      .sort("-createdAt")
+      .sort(sort)
       .skip(perPage * page - perPage)
       .limit(perPage)
-      .populate("category");
+      .populate('category');
 
     const count = await Product.count({ category: foundCategory.id });
 
-    res.render("shop/index", {
+    let queryParams = Object.entries(req.query)
+      .filter((entry) => entry[0] !== 'page')
+      .map((entry) => entry.join('='))
+      .join('&');
+
+    if (queryParams) queryParams += '&';
+
+    const bc = req.breadcrumbs.map((b) => {
+      return { ...b, name: b.name.split('?')[0] };
+    });
+
+    res.render('shop/index', {
+      sort,
       pageName: foundCategory.title,
       currentCategory: foundCategory,
       products: allProducts,
       successMsg,
       errorMsg,
       current: page,
-      breadcrumbs: req.breadcrumbs,
-      home: "/products/" + req.params.slug.toString() + "/?",
+      breadcrumbs: bc,
+      home: '/products/' + req.params.slug.toString() + '/?' + queryParams,
       pages: Math.ceil(count / perPage),
     });
   } catch (error) {
     console.log(error);
-    return res.redirect("/");
+    return res.redirect('/');
   }
 });
 
 // GET: display a certain product by its id
-router.get("/:slug/:id", async (req, res) => {
-  const successMsg = req.flash("success")[0];
-  const errorMsg = req.flash("error")[0];
+router.get('/:slug/:id', async (req, res) => {
+  const successMsg = req.flash('success')[0];
+  const errorMsg = req.flash('error')[0];
   try {
-    const product = await Product.findById(req.params.id).populate("category");
-    res.render("shop/product", {
+    const product = await Product.findById(req.params.id).populate('category');
+    res.render('shop/product', {
       pageName: product.title,
       product,
       successMsg,
@@ -118,7 +149,7 @@ router.get("/:slug/:id", async (req, res) => {
     });
   } catch (error) {
     console.log(error);
-    return res.redirect("/");
+    return res.redirect('/');
   }
 });
 
